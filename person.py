94f095010cc951352e8e4c5e4c8f28ea12b074d7
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-15 23:32:11 -0400

    finsihed step 3

--- person.py
+++ person.py
@@ -4,13 +4,23 @@ class Person:
         self.job = job
         self.pay = pay
 
+    def last_name(self):
+        return self.name.split()[-1]
+
+    def give_raise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+    def __repr__(self):
+        return "[Person: {}, ${}]".format(self.name, self.pay)
+
 
 if __name__ == "__main__":  # run tests at bottom only when file is run
     bob = Person("Bob Smith")  # gets the defaults
     sue = Person("Sue Jones", job="dev", pay=100000)
-    print(bob.name, bob.pay)  # to get rid of the extra () --> format/fstring
-    print(sue.name, sue.pay)
-    print(bob.name.split()[-1])  # last name
-    sue.pay *= 1.10  # inreases pay
-    print("%.2f" % sue.pay)
-    # print("{}".format(sue.pay))
+    # print(bob.name, bob.pay)  # to get rid of the extra () --> format/fstring
+    # print(sue.name, sue.pay)
+    print(bob)
+    print(sue)
+    print(bob.last_name(), sue.last_name())
+    sue.give_raise(.10)
+    print(sue)
